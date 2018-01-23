98a22ab33852240c73cb60c840351797277e44f6
GaCampioto <gcampioto@gmail.com>
2018-01-23 21:51:27 -0200

    add: injection by constructor

--- src/main/java/br/com/gabriel/jokesapp/service/JokeService.java
+++ src/main/java/br/com/gabriel/jokesapp/service/JokeService.java
@@ -1,14 +1,16 @@
 package br.com.gabriel.jokesapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeService {
 
-    @Autowired
     ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
 
     public String getChuckNorrisQuote() {
         return chuckNorrisQuotes.getRandomQuote();
