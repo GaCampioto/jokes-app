22ce137fac69c738d396e787165afcba724587c5
GaCampioto <gcampioto@gmail.com>
2018-01-23 21:19:48 -0200

    add: joke service

---
+++ src/test/java/br/com/gabriel/jokesapp/service/JokeServiceTest.groovy
@@ -0,0 +1,24 @@
+package br.com.gabriel.jokesapp.service
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes
+import spock.lang.Specification
+
+class JokeServiceTest extends Specification {
+
+    JokeService jokeService
+    ChuckNorrisQuotes chuckNorrisQuotesMock
+
+    void setup() {
+        chuckNorrisQuotesMock = Mock(ChuckNorrisQuotes)
+        jokeService = new JokeService(chuckNorrisQuotes: chuckNorrisQuotesMock)
+    }
+
+    def "should return a random chuck norris quote"() {
+        when:
+            String quote = jokeService.getChuckNorrisQuote()
+        then:
+            1 * chuckNorrisQuotesMock.getRandomQuote() >> 'RANDOM QUOTE'
+            !quote.isEmpty()
+            quote
+    }
+}
